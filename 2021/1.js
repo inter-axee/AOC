e3f717b3986110c76123892c78fc14b116c2b2dc
axel-seugling <axel.seugling@inter.ikea.com>
2021-12-01 06:34:44 +0100

    problem 1 complete

--- 2021/1.js
+++ 2021/1.js
@@ -1,11 +1,32 @@
 const {readInFile} = require('../read_input');
 
 const input = parseInput(readInFile());
+//console.log(input);
+function calculate(problemA){
+    let first = parseInt(input[0]);
+    if (!problemA) 
+        first = parseInt(input[0]) + parseInt(input[1]) + parseInt(input[2]);
+    let result = 0;
+    for(let i = 1; i < input.length-2; i++){
+        let second = parseInt(input[i])
+        if (!problemA) 
+            second = parseInt(input[i]) + parseInt(input[i+1]) + parseInt(input[i+2]);
+        if (second > first)
+            result++;
+        first = second;
+        //console.log(input[i]);
+    }
+    return result;
+    //console.log(result);
+    }
 
-console.log(input);
+//Problem A
+console.log(calculate(true));
+//Problem B
+console.log(calculate(false));
 
 function parseInput(input_string){
-    return input_string.split(',').map(line => {
+    return input_string.split('\n').map(line => {
         return line.trim();
     });
 }
