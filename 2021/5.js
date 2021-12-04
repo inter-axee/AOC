d6088cee2448a307b3624010a40175c18ab5459e
axel-seugling <axel.seugling@inter.ikea.com>
2021-12-04 11:00:22 +0100

    Prepare day 5

---
+++ 2021/5.js
@@ -0,0 +1,24 @@
+const {readInFile} = require('../read_input');
+
+const input = parseInput(readInFile());
+//console.log(input);
+function calculate(problemA){
+    
+    let result = 0;
+    for(let i = 0; i < input.length; i++){
+      
+    }
+    return result;
+    //console.log(result);
+}
+
+//Problem A
+console.log(calculate(true));
+//Problem B
+console.log(calculate(false));
+
+function parseInput(input_string){
+    return input_string.split(',').map(line => {
+        return line.trim();
+    });
+}
