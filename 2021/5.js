5b8ccc4eabe81794569d9661f0aaa5d1538b588b
axel-seugling <axel.seugling@inter.ikea.com>
2021-12-05 09:03:39 +0100

    Day 5 done

--- 2021/5.js
+++ 2021/5.js
@@ -2,14 +2,42 @@ const {readInFile} = require('../read_input');
 
 const input = parseInput(readInFile());
 //console.log(input);
+
+let vectors = [];
+for (let i = 0; i < input.length;i++)
+    vectors[i] = input[i].split('->').map(y=>y.split(',').map(x=>+x));
+
+const getDirection = (a, b) => a == b ? 0 : (a < b ?  1 : -1); 
+
 function calculate(problemA){
-    
-    let result = 0;
-    for(let i = 0; i < input.length; i++){
-      
+    let map = new Map();
+    for (let i = 0; i < vectors.length;i++){
+        let point1 = vectors[i][0];
+        let point2 = vectors[i][1];
+        let dx = getDirection(point1[0],point2[0]);
+        let dy = getDirection(point1[1],point2[1]);
+        while (true){
+            //only consider straight lines in problem A
+            if (problemA && (point1[0] != point2[0] && point1[1] != point2[1]))
+                break;
+
+            if (map.has(point1.join()))
+                map.set(point1.join(), map.get(point1.join())+1);
+            else
+                map.set(point1.join(), 1);
+
+            if (point1.join() == point2.join())
+                break;
+
+            point1 = [point1[0]+dx, point1[1]+dy];
+        }
     }
-    return result;
-    //console.log(result);
+    let count = 0;
+    for (let i of map.values()){
+     if (i >= 2)
+        count++;
+    }
+    return count;
 }
 
 //Problem A
@@ -18,7 +46,7 @@ console.log(calculate(true));
 console.log(calculate(false));
 
 function parseInput(input_string){
-    return input_string.split(',').map(line => {
+    return input_string.split('\n').map(line => {
         return line.trim();
     });
 }
