f7af483cfbaf36be6f785e57842702ca91e31c09
Axel Seugling <axel.seugling@inter.ikea.com>
2021-12-11 20:52:02 +0100

    Common functions created

--- 2021/0.js
+++ 2021/0.js
@@ -1,23 +1,28 @@
 const {readInFile} = require('../read_input');
+const {primeFactors,isPrime,generatePrime, printMatrix, createMatrixFrame} = require('../common');
 
 const input = parseInput(readInFile());
 //console.log(input);
 function calculate(problemA){
     let result = 0;
-    for(let i = 1; i < input.length; i++){
-     
-    }
-    return result;
+    printMatrix(input,8,4);
+    let m = createMatrixFrame(input,'#');
+    printMatrix(m,'#');
+    printMatrix(input);
+    /*for(let i = 1; i < input.length; i++){
+    }*/
+    return 0;
     //console.log(result);
 }
 
 //Problem A
 console.log(calculate(true));
 //Problem B
-console.log(calculate(false));
+//console.log(calculate(false));
 
 function parseInput(input_string){
-    return input_string.split(',').map(line => {
-        return line.trim();
+    return input_string.split('\n').map(line => {
+        //return line.trim();
+        return line.trim().split('').map(x=>+x);
     });
 }
---
+++ common.js
@@ -0,0 +1,97 @@
+'use strict'
+const colors = require('colors');
+
+/*
+ * Create a new instance of an array and create a frame
+ * around it with the value from frame
+ */
+const createMatrixFrame = (matrix, frame) => {
+    let newArray = matrix.map(function(arr) {
+        return arr.slice();
+    });
+    for (let i = 0; i < newArray.length; i++){
+        newArray[i].push(frame);
+        newArray[i].unshift(frame);
+    }
+    newArray.unshift( new Array(newArray[0].length).fill(frame) );
+    newArray.push( new Array(newArray[0].length).fill(frame) );
+    return newArray;
+}
+
+/**
+ * Debug function
+ * Print a matrix with integers and highlight 2 different numbers 
+ */
+const printMatrix = (matrix, highlight1, highlight2) => {
+    for (let i = 0; i < matrix.length; i++){
+        for (let j = 0; j < matrix[i].length; j++){
+            if (matrix[i][j] == highlight1)
+                process.stdout.write(matrix[i][j].toString().blue);
+            else if (matrix[i][j] == highlight2)
+            process.stdout.write(matrix[i][j].toString().red);
+            else
+                process.stdout.write(matrix[i][j].toString());
+        }
+        process.stdout.write('\n');
+    }
+}
+
+/**
+ * Returns array with all prime factors that number consists of.
+ * 
+ */
+const primeFactors = (number) => {
+    var factors = [],
+        divisor = 2;
+    while(number > 2) {
+        if(number % divisor == 0) {
+            factors.push(divisor);
+            number = number/divisor;
+        } else{
+            divisor++;
+        }
+    }
+    return factors;
+}
+
+const isPrime = (num) => {
+    for(let i = 2; i < num; i++)
+      if(num % i === 0) return false;
+    return num > 1;
+}
+
+/*
+ * Uses Sieve of Eratosthenes algoritm to calculate all prime number up to n.
+ * https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+ */
+const generatePrime = (n) => {
+    // Eratosthenes algorithm to find all primes under n
+    var array = [], upperLimit = Math.sqrt(n), output = [];
+    // Make an array from 2 to (n - 1)
+    for (var i = 0; i < n; i++) {
+        array.push(true);
+    }
+    // Remove multiples of primes starting from 2, 3, 5,...
+    for (var i = 2; i <= upperLimit; i++) {
+        if (array[i]) {
+            for (var j = i * i; j < n; j += i) {
+                array[j] = false;
+            }
+        }
+    }
+    // All array[i] set to true are primes
+    for (var i = 2; i < n; i++) {
+        if(array[i]) {
+            output.push(i);
+        }
+    }
+    return output;
+}
+
+module.exports = {
+    primeFactors,
+    isPrime,
+    generatePrime,
+    printMatrix,
+    createMatrixFrame
+}
