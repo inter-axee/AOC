042a399027c420658ac097d8854f1db3ce2af53f
axel-seugling <axel.seugling@inter.ikea.com>
2022-12-03 07:15:01 +0100

    Cleaned up and fixed cloning of array

--- 2022/3.js
+++ 2022/3.js
@@ -7,49 +7,47 @@ function calculate(problemA){
     input.forEach((a2) => {
         let length = a2.length
         let a1 = a2.splice(0,length/2);
-        //console.log(length + ' ' + a1 + '  2:'+  a2 );
-        const filteredArray = a1.filter(value => a2.includes(value));
-        //console.log(filteredArray);
-        unique = [...new Set(filteredArray)];
-        //console.log(unique);
-        unique.forEach(x => {
-            if (x.toUpperCase() == x){
-                //console.log(x.charCodeAt(0)-38);
-                result += x.charCodeAt(0)-38;
-            }
-            else{
-                //console.log(x.charCodeAt(0)-96);
-                result += x.charCodeAt(0)-96;
-            }
-        })
         
+        const filteredArray = a1.filter(value => a2.includes(value));
+        
+        unique = [...new Set(filteredArray)];
+        unique.forEach(x => {
+            if (x.toUpperCase() == x)
+                result += x.charCodeAt(0)-38;
+            else
+                result += x.charCodeAt(0)-96;
+        })        
     })
     return result;
 }
 
-//{}
 function calculate2(new_input){
     result = 0;
     for (let i = 0; i < new_input.length; i+=3){
         const tempArray = new_input[i].filter(value => new_input[i+1].includes(value));
         const filteredArray = tempArray.filter(value => new_input[i+2].includes(value));
-        //console.log(filteredArray);
+        
         unique = [...new Set(filteredArray)];
         unique.forEach(x => {
-            if (x.toUpperCase() == x){
-                //console.log(x.charCodeAt(0)-38);
+            if (x.toUpperCase() == x)
                 result += x.charCodeAt(0)-38;
-            }
-            else{
-                //console.log(x.charCodeAt(0)-96);
+            else
                 result += x.charCodeAt(0)-96;
-            }
         })
     }
     return result;
 }
 
-const savedInput = structuredClone(input);
+//const savedInput = structuredClone(input); //Only works on newer JS
+
+//Clone the input array
+const savedInput = [];
+input.forEach(x =>{
+    const t = [];
+    x.forEach(y => t.push(y));
+    savedInput.push(t)
+});
+
 //Problem A
 console.log(calculate());
 //Problem B
@@ -57,5 +55,5 @@ console.log(calculate2(savedInput));
 //console.log(calculate(false));
 
 function parseInput(input_string){
-    return input_string.split('\n').map(x=>x.split(''));
+    return input_string.split('\n').map(x=>x.trim().split(''));
 }
