897cfc778e63d7946e333c5b5d638fc27a4ffee6
Axel Seugling <axel.seugling@inter.ikea.com>
2022-12-03 07:00:14 +0100

    2022 day 3

---
+++ 2022/3.js
@@ -0,0 +1,64 @@
+const {readInFile} = require('../read_input');
+
+const input = parseInput(readInFile());
+
+function calculate(problemA){
+    let result = 0;
+    input.forEach((a2) => {
+        let length = a2.length
+        let a1 = a2.splice(0,length/2);
+        //console.log(length + ' ' + a1 + '  2:'+  a2 );
+        const filteredArray = a1.filter(value => a2.includes(value));
+        //console.log(filteredArray);
+        //{}
+        unique = [...new Set(filteredArray)];
+        //console.log(unique);
+        unique.forEach(x => {
+            if (x.toUpperCase() == x){
+                //console.log(x.charCodeAt(0)-38);
+                result += x.charCodeAt(0)-38;
+            }
+            else{
+                //console.log(x.charCodeAt(0)-96);
+                result += x.charCodeAt(0)-96;
+            }
+        })
+        
+    })
+    return result;
+}
+
+//{}
+function calculate2(new_input){
+    result = 0;
+    for (let i = 0; i < new_input.length; i+=3){
+        const tempArray = new_input[i].filter(value => new_input[i+1].includes(value));
+        const filteredArray = tempArray.filter(value => new_input[i+2].includes(value));
+        //console.log(filteredArray);
+        unique = [...new Set(filteredArray)];
+        unique.forEach(x => {
+            if (x.toUpperCase() == x){
+                //console.log(x.charCodeAt(0)-38);
+                result += x.charCodeAt(0)-38;
+            }
+            else{
+                //console.log(x.charCodeAt(0)-96);
+                result += x.charCodeAt(0)-96;
+            }
+        })
+    }
+    return result;
+}
+
+const savedInput = structuredClone(input);
+console.log("in: " + input[0].length + " sliced: " + savedInput[0].length);
+//Problem A
+console.log(calculate());
+console.log("in: " + input[0].length + " sliced: " + savedInput[0].length);
+//Problem B
+console.log(calculate2(savedInput));
+//console.log(calculate(false));
+
+function parseInput(input_string){
+    return input_string.split('\n').map(x=>x.split(''));
+}
